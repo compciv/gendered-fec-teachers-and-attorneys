04cf0b6cc192cb4054479197caac5b4ef53b9481
Dan Nguyen <dansonguyen@gmail.com>
2016-03-08 12:12:44 -0800

    ugh

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
+# Analyzing the laywers and teachers who donate to the FEC
+
+__Disclaimer:__ This project [is meant to be one of several example projects showing how effectively and industriously we can use public data to do gender classification](http://www.compciv.org/assignments/projects/gender-detector-data/) if we don't care about statistics or real-world complexities or computational efficiency. It is not meant to be an actual study or a display of programming best practices.
 
 
+Note: Results need to be recalculated...but the code pretty much works...
 
 
-
-
-# Facets of analysis
+# Analysis
 
 ## Teachers
 
--- analyze.py
+++ analyze.py
@@ -12,10 +12,15 @@ for year in YEARS:
 
         print(year)
         teacherrows = [r for r in genderedrows if 'TEACHER' in r['OCCUPATION']]
-        femaleteacherrows = [r for r in teacherrows if r['gender'] == 'F']
-        print( round(100 * (len(femaleteacherrows) / len(teacherrows))), "female teachers")
+        print("Teacher rows", len(teacherrows))
+#        femaleteacherrows = [r for r in teacherrows if r['gender'] == 'F']
+        print("TODO: get gender")
+
+        # print( round(100 * (len(femaleteacherrows) / len(teacherrows))), "female teachers")
 
 
         lawyerrows = [r for r in genderedrows if 'LAWYER' in r['OCCUPATION'] or 'ATTORNEY' in r['OCCUPATION']]
-        femalelawyerrows = [r for r in lawyerrows if r['gender'] == 'F']
-        print( round(100 * (len(femalelawyerrows) / len(lawyerrows))), "female attorneys")
+        print("TODO: get gender")
+        print("Lawyer rows", len(lawyerrows))
+        # femalelawyerrows = [r for r in lawyerrows if r['gender'] == 'F']
+        # print( round(100 * (len(femalelawyerrows) / len(lawyerrows))), "female attorneys")
